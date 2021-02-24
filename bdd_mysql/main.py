b0942010555ad93a2281c3bcb42d441bf1ec11b8
carinak6 <kassiscarina@gmail.com>
2021-02-24 19:46:20 +0100

    create table video

--- bdd_mysql/main.py
+++ bdd_mysql/main.py
@@ -5,11 +5,13 @@ from management_bdd import *
 
 def main():
     print('Pret!!')
-    """ liste_videos=[]
-    liste_videos.append('https://www.youtube.com/watch?v=m6chqKlhpPo','30 Days of Python - Day 15 - Automated Video Processing with Moviepy - Python TUTORIAL','autheur 1', 2020, 'description', 'Python') """
+    #liste_videos=[]
+    #liste_videos.append(['https://www.youtube.com/watch?v=m6chqKlhpPo','30 Days of Python - Day 15 - Automated Video Processing with Moviepy - Python TUTORIAL','autheur 1', 2020, 'description', 'Python'])
+    #INSERT INTO videos(titre, author,lien,anne_video,description,categorie) VALUES('30 Days of Python - Day 15 - Automated Video Processing with Moviepy - Python TUTORIAL','Auttheur1','https://www.youtube.com/watch?v=m6chqKlhpPo', '2020',"description dddd",'Python'),('Déboguer son code JavaScript : RegeneratorRuntime is not defined','auteur2','https://www.youtube.com/watch?v=upfsiT_A33I&list=PLjwdMgw5TTLWWXgsHpfCLHJ1Oq4YnE08e','2020','Javascript') 
     connect_bdd = ManagementBdd()
     connect_bdd.message()
-    """ connect_bdd.create_table_videos() """
+    connect_bdd.create_table_videos()
+    print('OK')
 
 
 if __name__ == "__main__":
