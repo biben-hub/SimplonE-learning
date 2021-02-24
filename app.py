c935969a42319d589eda8cb8cbda5fd232ca8b93
biben-hub <bilombob@gmail.com>
2021-02-24 15:22:18 +0100

    route index et add video

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask import redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'hello simplon'
+    return render_template('index.html')
 
+@app.route('/addvideo')
+def add_video():
+    return render_template('addvideo.html')
 
 if __name__ == '__main__':
     app.run()
