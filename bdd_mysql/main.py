728c0fcc733561fef23065b148fe1daaa0697839
carinak6 <kassiscarina@gmail.com>
2021-02-25 15:52:21 +0100

    ajouter les logs sur les fichiers bdd

--- bdd_mysql/main.py
+++ bdd_mysql/main.py
@@ -1,20 +1,32 @@
 import mysql.connector
 import json
+import logging
 
 from management_bdd import *
+
+logging.basicConfig(filename='logging_bdd.log', level=logging.INFO, format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 def main():
     print('Pret!!')
     liste_videos= ('Comprendre les modèles de Cloud (IaaS, PaaS, SaaS, CaaS, FaaS)', 'Cookie connecté','https://www.youtube.com/watch?v=Al-E4C69UmQ', '2020',"Dans cette vidéo, je vous explique les différents types de cloud :- Qu'est ce que le IaaS  (Infrastructure as a Service) ? - Qu'est ce que le PaaS  (Platform as a Service) ? - Qu'est ce que le SaaS  (Software as a Service) ?","Cloud")
     
     #INSERT INTO videos(titre, author,lien,anne_video,description,categorie) VALUES('30 Days of Python - Day 15 - Automated Video Processing with Moviepy - Python TUTORIAL','Auttheur1','https://www.youtube.com/watch?v=m6chqKlhpPo', '2020',"description dddd",'Python'),('Déboguer son code JavaScript : RegeneratorRuntime is not defined','auteur2','https://www.youtube.com/watch?v=upfsiT_A33I&list=PLjwdMgw5TTLWWXgsHpfCLHJ1Oq4YnE08e','2020','Javascript') 
+    logging.info("Appel de connection à la classe ManagementBdd - Start")
     connect_bdd = ManagementBdd()
     #connect_bdd.message()
+    logging.info("create database - Start")
     connect_bdd.create_database()
-    connect_bdd.create_table_videos()
-    connect_bdd.insert_donnes(liste_videos)
-    print('Fin du Script)
+    logging.info("create database - End")
 
+    logging.info("create table videos - Start")
+    connect_bdd.create_table_videos()
+    logging.info("create table videos - End")
+
+    logging.info("Insertion des donnes - Start")
+    connect_bdd.insert_donnes(liste_videos)
+    logging.info("Insertion des donnes - End")
+    print('Fin du Script')
+    logging.info("Fin du Script")
 
 if __name__ == "__main__":
     main()
--- bdd_mysql/management_bdd.py
+++ bdd_mysql/management_bdd.py
@@ -1,5 +1,7 @@
 import mysql.connector
+import logging
 
+logging.basicConfig(filename='logging_bdd.log', level=logging.INFO, format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 #create database e_learning;
 class ManagementBdd:
@@ -7,12 +9,15 @@ class ManagementBdd:
         
         #"Établissement de la connexion - Création du curseur"
         try:
+            logging.info("class ManagementBdd, methode: init - Start")
             self.cnx = mysql.connector.connect(user='cary', password='cordoba#1234AA', host='localhost', database='e_learning', use_unicode=True, charset='utf8')
-            print('connexion reussi') 
+            print('connexion reussi')
+            logging.info("connection reussi à la BDD") 
             #self.mycursor = self.cnx.cursor()
             self.echec =0
-
+            logging.info("class ManagementBdd, methode: init - End")
         except mysql.connector.Error as err:
+            logging.error("Error de connection "+ err)
             print("Something went wrong, La connexion avec la base de données a échoué : {}".format(err))       
             self.echec =1        
             
@@ -21,59 +26,73 @@ class ManagementBdd:
     
     def create_database(self):
         try:       
-
+            logging.info("class ManagementBdd, methode: create_database - Start")
             var_cursor = self.cnx.cursor()
             
             self.requete=f'CREATE DATABASE IF NOT EXISTS e_learning'
 
+            #logging.info("create_database requete : "+self.requete)
             var_cursor.execute(self.requete)
             print('creation BDD reussi!!!')
+            logging.info("creation BDD reussi!!!")
+            logging.info("class ManagementBdd, methode: create_database, creation BDD reussi!!! - End")
+
             return 1
 
         except Exception as err:
             # afficher la requête et le message d'erreur système :
             print ("Requête SQL incorrecte :\n%s\nErreur détectée :\n%s"\
                 % (self.requete, err))
+            logging.error("Error - class ManagementBdd, methode: create_database "+self.requete+ ' - '+err)
             return 0   
     
     def create_table_videos(self):
         try:       
-
+            logging.info("class ManagementBdd, methode: create_table_videos - Start")
             var_cursor = self.cnx.cursor()
             
             self.requete=f'CREATE TABLE IF NOT EXISTS videos (video_id INTEGER AUTO_INCREMENT PRIMARY KEY, titre VARCHAR(250) NOT NULL, author VARCHAR(250), lien TEXT NOT NULL, anne_video VARCHAR(4), description TEXT, categorie VARCHAR(200)) '
+            #logging.info("create_table_videos, requete : "+self.requete)
 
             var_cursor.execute(self.requete)
             print('creation reussi!!!')
+            logging.info("creation reussi!!!")
+            logging.info("class ManagementBdd, methode: create_table_videos - End")
             return 1
 
         except Exception as err:
+            logging.error("Error - class ManagementBdd, methode: create_table_videos "+self.requete+ ' - '+err)
             # afficher la requête et le message d'erreur système :
             print ("Requête SQL incorrecte :\n%s\nErreur détectée :\n%s"\
                 % (self.requete, err))
             return 0         
     
-    def insert_donnes(self, donnes):
+    def insert_donnes(self, donnes):        
+
         #donnes sera un dictionnare json envoyé pour le flask
         #print(donnes, type(donnes))#<class 'list'>
         try:
+            logging.info("class ManagementBdd, methode: insert_donnes - Start")
             mon_cursor = self.cnx.cursor()
             #print(donnes)           
 
-            #on cree la requete d'insertion
-            #['https://www.youtube.com/watch?v=m6chqKlhpPo','30 Days of Python - Day 15 - Automated Video Processing with Moviepy - Python TUTORIAL','autheur 1', 2020, 'description', 'Python']
+            #on cree la requete d'insertion            
             sql_insert = ('INSERT INTO videos(titre, author,lien,anne_video,description,categorie) VALUES(%s,%s,%s,%s,%s,%s)')
             
+            #logging.info("insert_donnes , requete : "+sql_insert)
             #on execute le methode pour inserer les donnes sur la BDD bd_scrapping)
             mon_cursor.execute(sql_insert, donnes) #execute le curseur avec la methode executemany transmit la requete
             
             self.cnx.commit() #valide la transaction
                 
-            print(mon_cursor.rowcount, "record inserted.\n")           
-        
+            print(mon_cursor.rowcount, "record inserted.\n")  
+
+            logging.info (str(mon_cursor.rowcount) + " record inserted.\n")   
+            logging.info("class ManagementBdd, methode: insert_donnes - End")
 
         except mysql.connector.Error as err:
             print("Something went wrong, un erreur se produit : {}".format(err))
+            logging.error("Error - class ManagementBdd, methode: insert_donnes "+self.requete+ ' - '+err)
 
     
 
