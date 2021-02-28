c45cddc02ee4d980c24cf16cd9844b0421c45c04
Adlane <50279061+aaadlane@users.noreply.github.com>
2021-02-28 19:54:22 +0100

    test docker compose

--- flask/app.py
+++ flask/app.py
@@ -3,7 +3,14 @@ from flask import redirect, jsonify
 import mysql.connector
 
 
-mybdd= mysql.connector.connect(user='cary', password='cordoba#1234AA', host='localhost', database='e_learning', use_unicode=True, charset='utf8')
+mybdd = mysql.connector.connect(
+    host="cloudflix_host",
+    user="root",
+    database="cloudflix_db",
+    password="cloudflix123",
+    use_unicode=True,
+    charset='utf8'
+)
 
 
 app = Flask(__name__)
@@ -15,43 +22,41 @@ def index():
     sql = "SELECT * FROM videos"
     cursor.execute(sql)
     results = cursor.fetchall()
-    return render_template('index.html', results = results, len=len(results))
+    return render_template('index.html', results=results, len=len(results))
 
 
 @app.route('/addvideo')
 def add_video():
     return render_template('addvideo.html')
-    
-@app.route('/upload_video', methods = ["POST", "GET"])
+
+
+@app.route('/upload_video', methods=["POST", "GET"])
 def upload_video():
 
     if request.method == "POST":
-        lien           = request.form.get('lien')
-        categorie      = request.form.get('categorie')
-        titre          = request.form.get('titre')
-        author         = request.form.get('author')
-        description    = request.form.get('description')
-        anne_video     = request.form.get('anne_video')
+        lien = request.form.get('lien')
+        categorie = request.form.get('categorie')
+        titre = request.form.get('titre')
+        author = request.form.get('author')
+        description = request.form.get('description')
+        anne_video = request.form.get('anne_video')
 
         cursor = mybdd.cursor()
         cursor.execute('''INSERT INTO  videos
                         (titre, author, lien, anne_video, description, categorie) 
                         VALUES (%s, %s, %s, %s, %s, %s)''',
-                        (lien, categorie, titre, author, description, anne_video))
+                       (lien, categorie, titre, author, description, anne_video))
 
         mybdd.commit()
     return redirect('/')
 
 
-
-
-
-@app.route('/apivideo',methods=['GET'])
+@app.route('/apivideo', methods=['GET'])
 def api_video():
-    dict_videos=[]
+    dict_videos = []
     cursor = mybdd.cursor(dictionary=True)
-    print('Curseur --->',cursor)
-    sql='select * from videos'
+    print('Curseur --->', cursor)
+    sql = 'select * from videos'
     cursor.execute(sql)
 
     for i in cursor:
@@ -60,5 +65,6 @@ def api_video():
     resp = jsonify(dict_videos)
     return resp
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=4000, debug=True)
